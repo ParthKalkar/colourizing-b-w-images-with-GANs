e7fecc939a33f7d6aee38e1f30810f8a1d32bdd4
dequinox <umarov.doro@gmail.com>
2022-10-04 18:27:42 +0300

    feat: backend with 1 endpoint

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+ERR_NO_FILE = "no file selected"
+APP_OK = "operation successful"
---
+++ backend/utils.py
@@ -0,0 +1,5 @@
+from config import *
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
